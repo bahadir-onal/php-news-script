a15cb45617b8a63b8cc86a07d1b371849585bfd8
bahadir-onal <bahadironal3@gmail.com>
2021-11-03 14:48:45 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -13,7 +13,7 @@ $db=new crud();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>NE YAZILIM CMS</title>
+  <title>Bahadır Önal</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
